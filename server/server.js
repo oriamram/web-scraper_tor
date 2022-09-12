d1a1c9719a4be22a89abdbc6a081a35788a15e05
student <oriamram@cyber4s.dev>
2022-09-12 20:58:27 +0300

    added ws and mongo

--- server/server.js
+++ server/server.js
@@ -1,12 +1,27 @@
 const express = require("express");
-const databaseManager = require("./databaseManager/databaseManager");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
+const io = require("socket.io")(4545, {
+	cors: {
+		origin: ["http://localhost:3000"],
+	},
+});
+const databaseManager = require("./databaseManager/databaseManager");
 const PORT = process.env.PORT || 4000;
-
 const db = new databaseManager();
+app.use(cors({ origin: "http://localhost:3000" }));
+
+io.on("connection", (socket) => {
+	console.log(socket.id);
+	socket.on("custom", (number) => {
+		console.log(number);
+	});
+	io.emit("fromServer", 100);
+});
 
 app.get("/", async (req, res) => {
-	res.send(await db.getPasteByName("pp"));
+	res.send(await db.getAllPastes());
 });
 
 app.listen(PORT, () => console.log(`listen on ${PORT}`));
